fd3ca3cc9aaa9d03468603eb7429c9fd513cdf64
Rahul Patni <rpatni@purdue.edu>
2016-07-21 12:38:57 -0400

    Adding controllers

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name = "Rahul", int numTimes = 5)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
